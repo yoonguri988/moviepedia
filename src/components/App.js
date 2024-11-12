9649ece4524dd50aeeadb440b455cf743d2772d5
CHOI YOON JEONG <56820897+yoonguri988@users.noreply.github.com>
2024-11-13 08:17:06 +0900

    Feat: sort로 정렬 바꾸기 - 빠진 파일 존재 (#4)
    - 파일 복원
    1. 정렬 기준을 선택할 수 있게
    2. 사용자가 선택한 기준으로 items를 정렬
    3. 버튼을 통해 사용자가 정렬 기준을 선택

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
 import ReviewList from "./ReviewList";
 import items from "../mock.json";
+import { useState } from "react";
 
 function App() {
+  // 정렬 기준을 선택할 수 있도록
+  const [order, setOrder] = useState("createdAt");
+  // 별점이 높은 순서대로 정렬
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  // State 값을 사용자가 선택
+  const handleNewestClick = () => setOrder("createdAt");
+  const handleBestClick = () => setOrder("rating");
   return (
     <div>
-      <ReviewList items={items} />
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleBestClick}>별점순</button>
+      </div>
+      <ReviewList items={sortedItems} />
     </div>
   );
 }
