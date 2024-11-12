466c3c58af76ad47f044cca75e62013b54237e24
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-11-13 08:14:14 +0900

    Feat: sort로 정렬 바꾸기
    - 파일 복원
    1. 정렬 기준을 선택할 수 있게
    2. 사용자가 선택한 기준으로 items를 정렬
    3. 버튼을 통해 사용자가 정렬 기준을 선택

--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -5,7 +5,7 @@ function formatDate(value) {
   return `${date.getFullYear()}. ${date.getMonth() - 1}. ${date.getDate()}`;
 }
 
-function ReviewListItem({ item }) {
+function ReviewListItem({ item, onDelete }) {
   return (
     <div className="ReviewListItem">
       <img className="ReviewListItem-img" src={item.imgUrl} alt={item.title} />
@@ -14,12 +14,13 @@ function ReviewListItem({ item }) {
         <p>{item.rating}</p>
         <p>{formatDate(item.createdAt)}</p>
         <p>{item.content}</p>
+        <button onClick={handleDeleteClick}>삭제</button>
       </div>
     </div>
   );
 }
 
-function ReviewList({ items }) {
+function ReviewList({ items, onDelete }) {
   return (
     <ul>
       {items.map((item) => {
