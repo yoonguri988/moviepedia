a4a9d4f133be27dea05a42d9e67e06f7684ccc42
CHOI YOON JEONG <56820897+yoonguri988@users.noreply.github.com>
2024-11-25 01:48:05 +0900

    Feat/pagination: 페이지네이션 (#8)
    * 78d728f Fix: 비동기로 state변경시 주의
    * 35778f2 Fix: 데이터가 있을때만 데이터 보여주기
    * eee79dd Feat: 데이터 더 불러오기

--- src/api.js
+++ src/api.js
@@ -2,8 +2,12 @@
  * 리퀘스트 함수들을 작성
  */
 const API = 1717;
-export async function getReviews(order = "createdAt") {
-  const query = `order=${order}`;
+export async function getReviews({
+  order = "createdAt",
+  offset = 0,
+  limit = 10,
+}) {
+  const query = `order=${order}&offset=${offset}&limit=${limit}`;
   const response = await fetch(
     `http://learn.codeit.kr/${API}/film-reviews?${query}`
   );
