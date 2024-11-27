ec7f392b2b396ad79e99110336d87664fcced2be
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-11-27 16:17:47 +0900

    Feat: file input

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,20 +1,27 @@
 import { useState } from "react";
 import "./ReviewForm.css";
+import FileInput from "./FileInput";
 
 function ReviewForm() {
   const [values, setValues] = useState({
     title: "",
     rating: 0,
     context: "",
+    imgFile: null,
   });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+  const handleChange = (name, value) => {
     setValues((prevValues) => ({
       ...prevValues,
       [name]: value,
     }));
   };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    handleChange(name, value);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(values);
@@ -22,19 +29,28 @@ function ReviewForm() {
 
   return (
     <form className="ReviewForm" onSubmit={handleSubmit}>
+      <FileInput
+        name="imgFile"
+        value={values.imgFile}
+        onChange={handleChange}
+      />
       <input
         type="text"
         value={values.title}
         name="title"
-        onChange={handleChange}
+        onChange={handleInputChange}
       />
       <input
         type="number"
         value={values.rating}
         name="rating"
-        onChange={handleChange}
+        onChange={handleInputChange}
       />
-      <textarea name="context" value={values.context} onChange={handleChange} />
+      <textarea
+        name="context"
+        value={values.context}
+        onChange={handleInputChange}
+      />
       <button type="submit">확인</button>
     </form>
   );
