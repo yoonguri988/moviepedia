3f30b545017d282635f492122e0d2beaeaa90e89
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-11-27 16:32:17 +0900

    Feat: file input clear

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,4 +1,5 @@
-import { useEffect, useRef } from "react";
+// import { useEffect, useRef } from "react";
+import { useRef } from "react";
 
 function FileInput({ name, value, onChange }) {
   const inputRef = useRef();
@@ -7,10 +8,23 @@ function FileInput({ name, value, onChange }) {
     onChange(name, nextValue);
   };
 
+  const handleClearClick = () => {
+    const inputNode = inputRef.current;
+    if (!inputNode) return;
+
+    inputNode.value = "";
+    onChange(name, null);
+  };
+
   // useEffect(() => {
   //   if (inputRef.current) console.log(inputRef);
   // }, []);
-  return <input type="file" onChange={handleChange} ref={inputRef} />;
+  return (
+    <div>
+      <input type="file" onChange={handleChange} ref={inputRef} />
+      {value && <button onClick={handleClearClick}>X</button>}
+    </div>
+  );
 }
 
 export default FileInput;
